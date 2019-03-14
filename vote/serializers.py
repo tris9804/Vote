a18ad8c1bde705c0aa00fb0154e6aeffeea48280
tris <mea9096@gmail.com>
2019-03-14 21:49:11 +0800

    add 0314

--- vote/serializers.py
+++ vote/serializers.py
@@ -6,3 +6,5 @@ class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
         fields = '__all__'
+        read_only_fields = [ 'vote' ]
+
--- vote/views.py
+++ vote/views.py
@@ -1,7 +1,30 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Candidate
 from .serializers import CandidateSerializer
+from rest_framework.decorators import action
+
+
 
 class CandidateViewSet(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
+
+    # @action(['POST'], True)
+    # def creat_candidate(self, request):
+    #     candidate = self.get_object()
+    #     candidate.save()
+    #     return Response('新增成功')
+
+
+
+    @action(['PATCH'],True)
+    def vote(self, request, pk):
+        candidate = self.get_object()
+        candidate.vote += 1
+        candidate.save()
+        return Response('投票成功')
+        
+
+
+
